9493d8f0343414d773d652c397317419af83e76a
Snakevil Zen <zsnakevil@gmail.com>
2016-10-13 16:00:03 +0800

    调整清理过期静态缓存文件的计划任务脚本以适应新的缓存机制。

--- share/cron/drop-expired-html-cache.sh
+++ share/cron/drop-expired-html-cache.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+ROOT=var/cache
 cd `'dirname' $0`/../..
-'find' var/cache -type f -name '*.html' -mmin +0 -delete
+find "$ROOT" -type f -name '.*.expires' -mmin +0 | while read line; do
+  prefix=`dirname "$line"`
+  suffix=`basename "$line" .expires`
+  bin/cache-purge "$ROOT" "${prefix:10}/${suffix:1}"
+done
